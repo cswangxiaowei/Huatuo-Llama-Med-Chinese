8eda4edd7ad2948bf8739c6acd0db5609649d1b4
cswangxiaowei <145024452+cswangxiaowei@users.noreply.github.com>
2024-01-01 19:30:39 -0600

    Update infer-literature-single.sh

--- scripts/infer-literature-single.sh
+++ scripts/infer-literature-single.sh
@@ -7,7 +7,7 @@
 """单轮"""
 python infer_literature.py \
     --base_model 'decapoda-research/llama-7b-hf' \
-    --lora_weights './lora-llama-literature' \
+    --lora_weights '/content/drive/MyDrive/lora-llama-med' \
     --single_or_multi 'single' \
     --use_lora True \
     --prompt_template 'literature_template'
